cb79347efaac87566a00c5a916966642ed61cf28
HaMa1654 <l.maximilian.h@gmail.com>
2018-04-16 16:03:24 +0200

    Commit error fixes

---
+++ CVGPrototype/CVGPrototype/Controllers/RasorteringController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CVGPrototype.Controllers
+{
+    public class RasorteringController : Controller
+    {
+        // GET: Rasortering
+        public ActionResult Index()
+        {
+            return View("rasortering");
+        }
+    }
+}
