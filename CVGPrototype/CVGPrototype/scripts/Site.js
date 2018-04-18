d395374d3b733c24c2fd84aafa6598e0d5b6d515
henrik sandwall <henriksandwall@hotmail.com>
2018-04-18 11:16:59 +0200

    Section in rasortering

--- CVGPrototype/CVGPrototype/scripts/Site.js
+++ CVGPrototype/CVGPrototype/scripts/Site.js
@@ -1,9 +1,21 @@
 ﻿
 window.onscroll = function () {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("mainNav").classList.add("past-main")
+        document.getElementById("mainNav").classList.add("past-main");
     }
     else {
-        document.getElementById("mainNav").classList.remove("past-main")
+        document.getElementById("mainNav").classList.remove("past-main");
     }
 }
+
+/*function hide() {
+
+    var x = document.getElementById("hideDiv");
+    if (x.style.visibility == "visible") {
+        x.style.visibility = "hidden";
+    }
+    else {
+        
+        x.style.visibility = "visible";
+    }
+}*/
